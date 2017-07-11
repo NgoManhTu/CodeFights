45d0458d4206268fb4cc3f7453c382a7abf3a470
Vu Duc Nhi <vdn1999bxvp@users.noreply.github.com>
2017-07-11 20:06:58 +0700

    Create mergeKArrays.cpp

---
+++ InterviewPractice/Sorting/mergeKArrays.cpp
@@ -0,0 +1,23 @@
+std::vector<int> mergeKArrays(std::vector<std::vector<int>> arrays) {
+  std::vector<int> firstUnused(arrays.size(), 0),
+                   result;
+  int n = 0;
+  for (int i = 0; i < arrays.size(); i++) {
+    n += arrays[i].size();
+  }
+  for (int i = 0; i < n; i++) {
+    int minIndex = -1,
+        minValue = 0;
+    for (int j = 0; j < arrays.size(); j++) {
+      if (firstUnused[j] < arrays[j].size()) {
+        if (minIndex == -1 || minValue > arrays[j][firstUnused[j]]) {
+          minIndex = j;
+          minValue = arrays[j][firstUnused[j]];
+        }
+      }
+    }
+    result.push_back(minValue);
+    firstUnused[minIndex]++;
+  }
+  return result;
+}
