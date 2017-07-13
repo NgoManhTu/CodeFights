3764dd61c41542a3c57a3a05912eee61d1d3fd4f
Vu Duc Nhi <vdn1999bxvp@gmail.com>
2017-07-13 17:05:05 +0700

    update modInverse.cpp

--- InterviewPractice/Number Theory/modInverse.cpp
+++ InterviewPractice/Number Theory/modInverse.cpp
@@ -1,14 +1,14 @@
 long long modInverse(long long n , long long m) {
     if (n == 1 && m == 2) return 1LL;
     if (__gcd(n , m) != 1) return -1LL;
-    long long q, r, y0 = 0LL, y1 = 1LL, y, m0 = m;
+    long long q, r, b = 0LL, c = 1LL, a, md = m;
     while (n > 0) {
         q = m / n;
         r = m % n;
-        if (r == 0LL) return (y % m0 + m0) % m0;
-        y = y0 - y1 * q;
-        y0 = y1;
-        y1 = y;
+        if (r == 0LL) return (a % md + md) % md;
+        a = b - c * q;
+        b = c;
+        c = a;
         m = n;
         n = r;
     }
