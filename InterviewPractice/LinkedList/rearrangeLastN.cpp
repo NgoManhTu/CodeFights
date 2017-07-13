ecde8570649026cc2f93a5bf2053cb7f7f2bffb6
Vu Duc Nhi <vdn1999bxvp@users.noreply.github.com>
2017-07-13 09:06:28 +0700

    Create rearrangeLastN.cpp

---
+++ InterviewPractice/LinkedList/rearrangeLastN.cpp
@@ -0,0 +1,34 @@
+// Definition for singly-linked list:
+// template<typename T>
+// struct ListNode {
+//   ListNode(const T &v) : value(v), next(nullptr) {}
+//   T value;
+//   ListNode *next;
+// };
+//
+ListNode<int> * rearrangeLastN(ListNode<int> * l, int n) {
+    if (l == NULL) return NULL;
+    vector <int> v;
+    while (l != NULL) {
+        v.push_back(l-> value);
+        l = l-> next;
+    }
+    ListNode<int>* res = new ListNode<int>;
+    ListNode<int>* currrentNode = res;
+    ListNode<int>* previousNode = NULL;
+    for (int i = v.size() - n; i < v.size(); i++) {
+        currrentNode-> value = v[i];
+        currrentNode-> next = new ListNode<int>;
+        previousNode = currrentNode;
+        currrentNode = currrentNode-> next;
+    }
+    for (int i = 0; i < v.size() - n; i++) {
+        currrentNode-> value = v[i];
+        currrentNode-> next = new ListNode<int>;
+        previousNode = currrentNode;
+        currrentNode = currrentNode-> next;
+    }
+    previousNode-> next = NULL;
+    delete currrentNode;
+    return res;
+}
