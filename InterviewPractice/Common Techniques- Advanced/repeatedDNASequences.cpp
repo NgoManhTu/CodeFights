87deb7451712af61c0a5bc55ec3c78a748b2fc77
Vu Duc Nhi <vdn1999bxvp@users.noreply.github.com>
2017-07-15 17:01:10 +0700

    Update repeatedDNASequences.cpp

--- InterviewPractice/Common Techniques- Advanced/repeatedDNASequences.cpp
+++ InterviewPractice/Common Techniques- Advanced/repeatedDNASequences.cpp
@@ -1,1 +1,16 @@
-
+std::vector<std::string> repeatedDNASequences(std::string s) {
+    vector <string> res;
+    if (s.size() < 10) return res;
+    map <string, bool> exist;
+    map <string, bool> had;
+    for (int i = 0; i <= s.size() - 10; i++) {
+        string x = s.substr(i, 10);
+        if (exist[x] && !had[x]) {
+            res.push_back(x);
+            had[x] = true;
+        }
+        exist[x] = true;
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
