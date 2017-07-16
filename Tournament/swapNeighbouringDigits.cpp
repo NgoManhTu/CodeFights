76f9c5d351fd1a7bfe038ab317ff6a8a931ea38a
Vu Duc Nhi <vdn1999bxvp@gmail.com>
2017-07-16 18:14:21 +0700

    update

--- Tournament/swapNeighbouringDigits.cpp
+++ Tournament/swapNeighbouringDigits.cpp
@@ -1,12 +1,11 @@
 int swapNeighbouringDigits(int n) {
-    int result = 0,
-		tenPower = 1;
-	while (n != 0) {
-		int digit1 = n % 10,
-			digit2 = ((n - digit1) / 10) % 10;
-		result += tenPower * (10 * digit1 + digit2);
-		n = floor(n / 100);
-		tenPower *= 100;
-	}
-	return result;
+    int result = 0, tenPower = 1;
+  	while (n != 0) {
+	    	int digit1 = n % 10,
+			  digit2 = ((n - digit1) / 10) % 10;
+		    result += tenPower * (10 * digit1 + digit2);
+    		n = floor(n / 100);
+    		tenPower *= 100;
+  	}
+	  return result;
 }
